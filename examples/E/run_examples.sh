70b8b4d38f970438e3bfed98a60f488928667f90
test <test>
2024-11-06 15:57:01 -0700

    Add UMF examples

---
+++ examples/E/run_examples.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+#
+# shell commands to run this example
+#
+
+echo "Run dram_and_fsdax example"
+UMF_TESTS_FSDAX_PATH=/pmem/myfile ./dram_and_fsdax
+echo "-------------------------------------"
+
